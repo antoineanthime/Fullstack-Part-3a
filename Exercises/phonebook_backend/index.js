cb0c4a9f613a6f09b9dd03bfd17384491d0cc434
Antoine ANTHIME <aanthime@gmail.com>
2020-06-05 17:18:19 +0200

    Added Morgan support and configuration
    + Morgan middleware for logging requests
    + Morgan configuration for logging content of body in POST requests.

--- Exercises/phonebook_backend/index.js
+++ Exercises/phonebook_backend/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-
 app.use(express.json())
+
+morgan.token('content', function getContent(req) {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     {
